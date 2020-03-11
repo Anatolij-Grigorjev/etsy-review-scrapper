0d31a40d05e5912cfbe513bc8ca9a651bb95210c
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2020-03-11 13:06:42 +0200

    adjust script to use different URL source

--- src/etsy_scrubber/core.clj
+++ src/etsy_scrubber/core.clj
@@ -1,28 +1,40 @@
 (ns etsy-scrubber.core
-  (:require [clojure.string :as str]
-            [clj-http.client :as http])
+  (:require [clojure.string :as str])
   (:gen-class))
 
 (defn format-link
   "Surround link with required clicked bot formatting for script"
   [link]
-  (str "URL GOTO=" link (System/lineSeparator) 
-       "TAG POS=2 TYPE=A ATTR=TXT:Follow<SP>Loading" (System/lineSeparator) 
+  (str "URL GOTO=" link (System/lineSeparator)
+       "TAG POS=2 TYPE=A ATTR=TXT:Follow<SP>Loading" (System/lineSeparator)
        "WAIT SECONDS=2" (System/lineSeparator)))
 
 (def macro-header (str "VERSION BUILD=1005 RECORDER=CR" (System/lineSeparator)))
 
+(defn fetch-links-page
+  [base-url page-idx]
+  (def reviews-url (str base-url "?ref=pagination&page=" page-idx))
+  (println "Fetching reviews for " reviews-url)
+  (let [html (slurp reviews-url)
+        found-links (re-seq #"http.*ref=shop_review" html)]
+    found-links))
+
+
 (defn -main
   "Initialize scrapper with provided cookie and link"
   [& args]
-  (if (< (count args) 1) 
+  (if (< (count args) 1)
     (doall
-     (println "Need at least 1 argument to slurp HTML!")
+     (println "Need shop url to slurp reviews!")
      (System/exit 1)))
-  
-  (let [
-        html (slurp (first args))
-        found-links (re-seq #"http.*ref=shop_review" html)
-        formatted-links (map format-link found-links)]
-    (spit "new_script.iism" (str macro-header (apply str formatted-links)))
-    ))
+
+  (def shop-url-base (first args))
+  (def num-pages (or (Integer/parseInt (second args)) 25))
+  (def shop-name (let [shop-name-bit (re-find #"/shop/.+/" shop-url-base)]
+                   (subs shop-name-bit (count "/shop/") (dec (count shop-name-bit)))))
+
+  (let [fetched-links-lists (map #(fetch-links-page shop-url-base %) (range 1 (inc num-pages)))
+        all-links (set (flatten fetched-links-lists))
+        formatted-links (map format-link all-links)]
+    (println "Done fetching total of " (count all-links) " links!")
+    (spit (str shop-name "_" num-pages "_pages.iism") (str macro-header (apply str formatted-links)))))
