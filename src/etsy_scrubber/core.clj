9299141d08f1989547c29be3d32bc8f0be5dda42
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2020-03-11 11:59:35 +0200

    added initial scrapper logic for 1 page

--- src/etsy_scrubber/core.clj
+++ src/etsy_scrubber/core.clj
@@ -1,7 +1,28 @@
 (ns etsy-scrubber.core
+  (:require [clojure.string :as str]
+            [clj-http.client :as http])
   (:gen-class))
 
+(defn format-link
+  "Surround link with required clicked bot formatting for script"
+  [link]
+  (str "URL GOTO=" link (System/lineSeparator) 
+       "TAG POS=2 TYPE=A ATTR=TXT:Follow<SP>Loading" (System/lineSeparator) 
+       "WAIT SECONDS=2" (System/lineSeparator)))
+
+(def macro-header (str "VERSION BUILD=1005 RECORDER=CR" (System/lineSeparator)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Initialize scrapper with provided cookie and link"
   [& args]
-  (println "Hello, World!"))
+  (if (< (count args) 1) 
+    (doall
+     (println "Need at least 1 argument to slurp HTML!")
+     (System/exit 1)))
+  
+  (let [
+        html (slurp (first args))
+        found-links (re-seq #"http.*ref=shop_review" html)
+        formatted-links (map format-link found-links)]
+    (spit "new_script.iism" (str macro-header (apply str formatted-links)))
+    ))
